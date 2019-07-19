aa4c74b0eefb30f8e7d5cff874a7b5c8ef7b8e64
Michael Lin <unadlib@gmail.com>
2019-07-20 02:54:54 +0800

    fix crius-runner test

--- packages/crius-runner/test/index.test.ts
+++ packages/crius-runner/test/index.test.ts
@@ -3,10 +3,8 @@ import { run } from '../src';
 
 test('test run for step', async () => {
   class Foo extends Step { }
-  const foo = createFlow(Foo, {});
   class Bar extends Step { }
   const dosomething = async () => void await new Promise(resolve => setTimeout(resolve));
-  const bar = createFlow(Bar, {}, createFlow(foo, {}), dosomething);
   const caseStep = {
     key: undefined,
     props: {
