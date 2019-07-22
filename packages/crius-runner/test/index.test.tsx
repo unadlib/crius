41db78e330ac0d2164a3698c91752754202b1455
Michael Lin <unadlib@gmail.com>
2019-07-22 11:18:43 +0800

    add test for jsx

--- packages/crius-runner/test/index.test.tsx
+++ packages/crius-runner/test/index.test.tsx
@@ -1,28 +1,31 @@
-import Crius, { Step, FunctionStep } from 'crius';
+import Crius, { FunctionStep } from 'crius';
 import { run } from '../src';
 
 test('runner with JSX', async () => {
   const result: string[] = [];
   class Foo extends Crius.Step<{foo: string}> {
-    run() {
+    async run() {
+      await new Promise(resolve => setTimeout(resolve, 100));
       result.push(this.props.foo);
     }
   }
   class Bar extends Crius.Step<{ bar: string }> {
-    run() {
+    async run() {
+      await new Promise(resolve => setTimeout(resolve));
       result.push(this.props.bar);
       return this.props.children;
     }
   }
-
-  await run(<Foo foo='foo' />);
+  const FooBar: FunctionStep<{ fooBar: string }> = async (props) => result.push(props.fooBar);
+  await run(
+    <Bar bar='bar'>
+      <Foo foo='foo' />
+      <FooBar fooBar='fooBar' />
+    </Bar>
+  );
   expect(result).toEqual([
-    'foo'
+    'bar',
+    'foo',
+    'fooBar'
   ]);
-
-  // await run(<Bar bar='bar'><Foo foo='foo' /></Bar>);
-  // expect(result).toEqual([
-  //   'bar',
-  //   'foo'
-  // ]);
 });
