fd2146c4a842ac4976c1d943dadb091268cae40d
Michael Lin <unadlib@gmail.com>
2019-07-23 13:14:37 +0800

    add test

--- packages/crius-runner/test/context.test.ts
+++ packages/crius-runner/test/context.test.ts
@@ -50,3 +50,43 @@ test('base runner with extended step class for context', async () => {
     'bar',
   ]);
 });
+
+
+test('base runner with deep step class for context', async () => {
+  const result: string[] = [];
+  class Foo<P = {}, C = {}> extends Step<P & {foo: string}, C & { bar: string }> {}
+
+  class Foo1 extends Foo<{foo1: string}> {
+    async run() {
+      await new Promise(resolve => setTimeout(resolve));
+      result.push(this.context.bar);
+      return this.props.children;
+    }
+  }
+
+  class Bar extends Foo {
+    run() {
+      result.push(this.context.bar);
+    }
+  }
+
+  const caseStep = {
+    key: 'Foo1',
+    props: {
+      children: [
+        {
+          key: 'Foo1',
+          props: { children: [] },
+          step: Bar,
+        }
+      ],
+      foo: '1',
+    },
+    step: Foo1,
+  };
+  await run(caseStep, { bar: 'bar' });
+  expect(result).toEqual([
+    'bar',
+    'bar'
+  ]);
+});
