5e97c43b7842e83f70e019b43f58723dbbaeb4dd
Michael Lin <unadlib@gmail.com>
2019-07-25 15:49:56 +0800

    add crius-logger

--- packages/crius-test/src/builder.tsx
+++ packages/crius-test/src/builder.tsx
@@ -2,8 +2,6 @@ import { Step } from './step';
 
 type BaseProps<P, C> = { desc: string, action?: any }; // TODO fix type
 
-
-
 class Scenario<P = {}, C = {}> extends Step<P & BaseProps<P, C>, C> {
   run() {
     console.log(this.props.desc);
