8221f9da093be8c60a1c7f431122ea47c62af60b
Michael Lin <unadlib@gmail.com>
2019-07-29 16:30:35 +0800

    add build command

---
+++ packages/crius-graph/index.ts
@@ -0,0 +1,3 @@
+export {
+  graphParser
+} from './src';
--- packages/crius-logger/src/index.ts
+++ packages/crius-logger/src/index.ts
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import path from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
 import { Plugins } from 'crius-test';
 
 interface Log {
