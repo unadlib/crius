e937156a4b822f824793746e5a693b5817a4a2cb
unadlib <unadlib@gmail.com>
2019-08-14 13:31:03 +0800

    feat(example): add flow demo

--- packages/crius-example/test/step.ts
+++ packages/crius-example/test/step.ts
@@ -5,5 +5,5 @@ import {
 import logger from 'crius-logger';
 
 
-// @plugins([logger({ path: 'packages/crius-example/lib/log' })])
+@plugins([logger({ path: 'packages/crius-example/lib/log' })])
 export default class Step<P = {}, C = {}> extends BaseStep<P, C> {} 
